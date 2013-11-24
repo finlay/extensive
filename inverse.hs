0c3e6aee3e7e094c61afa197939ec7549f242dcb
Finlay Thompson <finlay@dragonfly.co.nz>
2013-11-24 22:03:23 +1300

    Added inverse test

---
+++ inverse.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import System.Random
+import Control.Monad
+
+import Numeric.Extensive
+
+-- Make a random matrix
+randomElement :: (FiniteSet a) => IO (V a)
+randomElement = 
+ do 
+    let sce b = fmap (flip scale (return b)) (randomRIO (-1, 1) )
+    cs <- mapM sce elements
+    return $ foldl1 plus cs
+
+randomMatrix :: (FiniteSet a, Eq a, FiniteSet b, Eq b) => IO (V a -> V b)
+randomMatrix = fmap apply randomElement
+
+data H = E | I | J | K deriving (Eq, Ord)
+instance FiniteSet H where elements = [ E, I, J, K ]
+
+main :: IO ()
+main = do 
+    
+    -- Generate random three by three linear transformation
+    --(a :: V H -> V H)  <- randomMatrix
+    (a :: V (Tensor H H) -> V (Tensor H H))  <- randomMatrix
+    putStrLn "A = "
+    printMap a
+    
+    let (ainv, ds) = inverse' a
+
+    putStrLn "A^{-1} = "
+    printMap ainv
+
+    putStrLn "Check..."
+    print (offNorm $ ainv . a)
+
+
+    putStrLn "Done"
+
+
+
+
