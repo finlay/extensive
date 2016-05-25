5f15baf20f78a0c9c7da917b4f5fcac7bae840ff
Finlay Thompson <finlay@dragonfly.co.nz>
2016-05-26 08:43:47 +1200

    Sketched out the new inverse function

--- src/Numeric/Extensive/Inverse2.hs
+++ src/Numeric/Extensive/Inverse2.hs
@@ -2,6 +2,25 @@ module Numeric.Extensive.Inverse2 where
 
 import Numeric.Extensive.Core
 
-
 inverse :: (V a -> V b) -> V b -> V a
-inverse = undefined
+inverse = complete . loop . initalise
+
+data LoopState 
+  = LS OffDiagonals (V a -> V b) (V b -> V a)
+
+intialise :: (V a -> V b) -> LoopState
+initalise l = LS (offDiagonals l) l (undefined)
+
+complete :: LoopState -> V b -> V a
+complete (LS _ _ out) = out
+
+done :: LoopState -> Bool
+done (LS diags _ _ ) = diagsAreZero diags
+
+loop :: LoopState -> LoopState
+loop ls = 
+    if done ls
+        then ls
+        else loop ls
+
+            
