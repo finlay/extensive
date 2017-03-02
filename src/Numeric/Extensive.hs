5a8ef6804a860b443237eb97fc7d73788a4b0375
Finlay Thompson <finlay@dragonfly.co.nz>
2017-03-03 00:18:00 +1300

    Progress on Inverse2

--- src/Numeric/Extensive.hs
+++ src/Numeric/Extensive.hs
@@ -1,13 +1,12 @@
 module Numeric.Extensive 
     ( module Numeric.Extensive.Core
     , module Numeric.Extensive.Print
-    , module Numeric.Extensive.Inverse
+    , module Numeric.Extensive.Inverse2
     ) 
     where
 
-
 import Numeric.Extensive.Core
 import Numeric.Extensive.Print
-import Numeric.Extensive.Inverse
+import Numeric.Extensive.Inverse2
 
 
--- src/Numeric/Extensive/Inverse2.hs
+++ src/Numeric/Extensive/Inverse2.hs
@@ -1,31 +1,76 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
 module Numeric.Extensive.Inverse2 where
 
+import Debug.Trace
 import Numeric.Extensive.Core
 
-inverse 
-  :: (Eq b, FiniteSet a, FiniteSet b) 
+--   \begin{align*}
+--         D     &= (A^TA)R              \\
+--     => Id     &= D^{-1}(A^TA)R        \\
+--     => Id     &= R^{-1}D^{-1}(A^TA)   \\
+--     => A^{-1} &= R^{-1}D^{-1}A^T      \\
+--     => A^{-1} &= (DR)^{-1}A^T      \\
+--   \end{align*}
+
+type End a = T a -> T a
+
+inverse2 
+  :: (Eq a, Eq b, Ord a, FiniteSet a, FiniteSet b, Show (T a -> T a)) 
   => (T a -> T b) -> T b -> T a
-inverse = complete . loop . initialise
+inverse2 a
+  = let at = transpose a
+        diags = [ (x,y) | x <- elements , y <- elements , x < y ]
+        ls = LS (at . a) id diags
+        (d,r) = decompose ls
+        recip' (T v) = T (recip . v)
+        dinv = recip' . d
+        rinv = transpose r
+    in  rinv . dinv . at
 
-data LoopState a b
-  = LS (V a -> V b) (V a -> V a)
+data LoopState a
+  = LS (End a)      -- transpose $ D := A^TA  $
+       (End a)      -- rotation  $ R := Id    $
+       [(a,a)]      -- diagonals that need checking
 
-initialise 
-  :: (Eq b, FiniteSet a, FiniteSet b) 
-  => (T a -> T b) -> LoopState a b
-initialise l 
-  = LS l (transpose l . l)
+isZero :: R -> Bool
+isZero r = abs r > 1e-8
 
-complete :: LoopState a b -> T b -> T a
-complete (LS _ out) = out
+decompose
+  :: (Eq a, FiniteSet a, Show (T a -> T a)) 
+  => LoopState a -> (End a, End a)
+decompose (LS d r []) = (d,r)
+decompose ls@(LS d r ((x,y):diags))
+  = let T dx = d (return x) 
+        c = traceShowId $ dx (delta y)
+        ls' = if isZero c 
+                then (LS d r diags) 
+                else loop c ls
+    in decompose ls'
 
--- Question about how to stop the loop
--- Lets try to skip this bit
-diagsAreZero :: (T b -> T b) -> Maybe (R, a,b)
-diagsAreZero e
-  = undefined
+loop
+  :: (Eq a, FiniteSet a, Show (T a -> T a)) 
+  => R -> LoopState a -> LoopState a
+loop _ (LS _ _ []) = error "Can't get here"
+loop c (LS d r ((x,y):diags))
+  = let rot = traceShowId $ makeRotation (angle c) x y
+        d' = d . rot
+        r' = (transpose rot) . r
+    in  LS d' r' (diags ++ [(x,y)])
 
-loop :: LoopState a b -> LoopState a b
-loop (LS l r) = undefined
+angle :: R -> R
+angle ct = 
+    let sgn a = a / abs a
+    in traceShowId $ atan $ (sgn ct) / ((abs ct) + (sqrt (1 + ct*ct)))
 
-            
+makeRotation
+  :: (Eq a, FiniteSet a) 
+  => R -> a -> a -> End a
+makeRotation t x y
+  = extend r
+  where 
+    st a = scale (sin t) (return a)
+    ct a = scale (cos t) (return a)
+    r i | x == i    =        ct x  `plus` st y
+        | y == i    = minus (st x) `plus` ct y
+        | otherwise = return i
