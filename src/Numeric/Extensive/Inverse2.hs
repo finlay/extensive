9633281b60ca6962763f67fd89dbc416e51a680d
Finlay Thompson <finlay@dragonfly.co.nz>
2017-03-12 10:28:16 +1300

    Got the inverse2 working

--- src/Numeric/Extensive/Inverse2.hs
+++ src/Numeric/Extensive/Inverse2.hs
@@ -6,27 +6,30 @@ import Debug.Trace
 import Numeric.Extensive.Core
 
 --   \begin{align*}
---         D     &= (A^TA)R              \\
---     => Id     &= D^{-1}(A^TA)R        \\
---     => Id     &= R^{-1}D^{-1}(A^TA)   \\
---     => A^{-1} &= R^{-1}D^{-1}A^T      \\
---     => A^{-1} &= (DR)^{-1}A^T      \\
+--         D     &= R^T(A^TA)R              \\
+--     => Id     &= D^{-1}R^T(A^TA)R        \\
+--     => Id     &= RD^{-1}R^T(A^TA)   \\
+--     => A^{-1} &= RD^{-1}R^TA^T      \\
 --   \end{align*}
 
 type End a = T a -> T a
 
 inverse2 
-  :: (Eq a, Eq b, Ord a, FiniteSet a, FiniteSet b, Show (T a -> T a)) 
+  :: (Show a, Eq a, Eq b, Ord a, 
+      FiniteSet a, FiniteSet b, Show (T a -> T a)) 
   => (T a -> T b) -> T b -> T a
 inverse2 a
   = let at = transpose a
         diags = [ (x,y) | x <- elements , y <- elements , x < y ]
         ls = LS (at . a) id diags
-        (d,r) = decompose ls
-        recip' (T v) = T (recip . v)
-        dinv = recip' . d
-        rinv = transpose r
-    in  rinv . dinv . at
+        (d,r) = loop ls
+    in  r . invDiagonal d . transpose r . at
+
+invDiagonal :: (Eq a) => End a -> End a
+invDiagonal l
+  = let coef x = let T v = l (return x) in v (delta x)
+        base x = scale (recip $ coef x) (return x)
+    in  extend base
 
 data LoopState a
   = LS (End a)      -- transpose $ D := A^TA  $
@@ -34,39 +37,38 @@ data LoopState a
        [(a,a)]      -- diagonals that need checking
 
 isZero :: R -> Bool
-isZero r = abs r > 1e-8
-
-decompose
-  :: (Eq a, FiniteSet a, Show (T a -> T a)) 
-  => LoopState a -> (End a, End a)
-decompose (LS d r []) = (d,r)
-decompose ls@(LS d r ((x,y):diags))
-  = let T dx = d (return x) 
-        c = dx (delta y)
-        ls' = if isZero c 
-                then (LS d r diags) 
-                else loop c ls
-    in decompose ls'
+isZero r = abs r < 1e-8
 
 loop
-  :: (Eq a, FiniteSet a, Show (T a -> T a)) 
-  => R -> LoopState a -> LoopState a
-loop _ (LS _ _ []) = error "Can't get here"
-loop c (LS d r ((x,y):diags))
-  = let rot = makeRotation (angle c) x y
-        d' = d . rot
-        r' = (transpose rot) . r
-    in  LS d' r' (diags ++ [(x,y)])
+  :: (Show a, Eq a, FiniteSet a, Show (T a -> T a)) 
+  => LoopState a -> (End a, End a)
+loop (LS d r []) = (d,r)
+loop ls@(LS d r ((x,y):diags))
+  = let T dx = d (return x)
+        c = dx (delta y)
+    in if isZero c
+          then loop (LS d r diags) 
+          else let rot = makeRotation d x y
+                   d' = transpose rot . d . rot
+               in  loop (LS d' (r . rot) (diags ++ [(x,y)]))
+
+makeRotation 
+    :: (Eq a, FiniteSet a)
+    => (T a -> T a) -> a -> a -> (T a -> T a)
+makeRotation m x y = 
+    let mc a b = let T mv = m (return a) in mv (delta b)
+        ct = ((mc x x) - (mc y y)) / (2*(mc x y))
+    in  rot x y (angle ct)
 
 angle :: R -> R
 angle ct = 
     let sgn a = a / abs a
     in atan $ (sgn ct) / ((abs ct) + (sqrt (1 + ct*ct)))
 
-makeRotation
+rot
   :: (Eq a, FiniteSet a) 
-  => R -> a -> a -> End a
-makeRotation t x y
+  => a -> a -> R -> End a
+rot x y t
   = extend r
   where 
     st a = scale (sin t) (return a)
@@ -76,5 +78,19 @@ makeRotation t x y
         | otherwise = return i
 
 
+-- Want to make a cheaper rotation, without transcendental functions
+makeRotation2
+  :: (Eq a, FiniteSet a) 
+  => R -> a -> a -> End a
+makeRotation2 t x y
+  | t < -1    =  extend (r (-t)       (-1))
+  | t > 1     =  extend (r   t          1 )
+  | otherwise =  extend (r   1   (recip t))
+  where
+    tx = return x
+    ty = return y
+    r a b i | x == i    = scale a    tx `plus` scale b ty
+            | y == i    = scale (-b) tx `plus` scale a ty
+            | otherwise = return i
 
 
