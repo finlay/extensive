9fc471f6f7742b20d5d30216e880424c1bf30865
Finlay Thompson <finlay@dragonfly.co.nz>
2017-04-02 01:49:57 +1300

    Fixed comment

--- centre.hs
+++ centre.hs
@@ -3,10 +3,27 @@
 -- Borrowing ideas from deGraaf
 
 import Prelude hiding ((+), (-), (*), (^), negate, (>), (<), sum, fromInteger)
-import qualified Prelude
+--import qualified Prelude
 import Numeric.Extensive
 import Numeric.Quaternion
 
+--  l . kernel l == zero
+kernel :: (T a -> T b) -> T (N n) -> T a
+kernel _l = undefined
+
+test :: T H -> T H
+test = extend t'
+  where 
+    t' E = return E
+    t' I = return I
+    t' J = zero
+    t' K = zero
+
+kernel_test :: T (N 2) -> T H
+kernel_test = extend t'
+  where 
+    t' (N 1) = return J
+    t' (N 2) = return K
 
 -- If we have an algebra, how do we calculate the centre ?
 -- And what does the answer look like ?
