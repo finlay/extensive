3a290c083314aa867f5a590c5a05f8f657dba33f
Finlay Thompson <finlay@dragonfly.co.nz>
2017-04-02 21:43:33 +1200

    Fixed filename

--- plot.r
+++ plot.r
@@ -3,9 +3,8 @@ args <- commandArgs(trailingOnly = TRUE)
 fn <- args[1]
 d <- read.csv(paste0(fn))
 
-        
-pdfn = paste0(gsub('.csv', '', fn), '.pdf')
-pdf(width=5, height=3, file=paste0(fn,'.pdf'))
+pdf(width=5, height=3, 
+    file=paste0(gsub('.csv', '', fn), '.pdf'))
 ggplot(data=d, aes(x=Name, group=1)) + 
     geom_line(aes(y=Mean)) + 
     geom_line(aes(y=MeanLB)) + 
