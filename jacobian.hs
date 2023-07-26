b4d487c8d419f72d4f64561de4404b9048ccbe47
Finlay Thompson <finlay@dragonfly.co.nz>
2023-07-26 17:31:49 +1200

    Added toDual

--- jacobian.hs
+++ jacobian.hs
@@ -19,22 +19,42 @@ br = extend $ hom . br'
 invbr :: T (Hom H H) -> T (Tensor H H)
 invbr = inverse br
 
+-- map Hom H H to Tensor H H*, where we equate H* with H (conjugated)
+toDual :: T (Hom H H) -> T (Tensor H H)
+toDual = extend toDual'
+  where
+    toDual' :: Hom H H -> T (Tensor H H)
+    toDual' (Hom E x) = return $ Tensor x E
+    toDual' (Hom y x) = scale (-1) $ return $ Tensor x y
+
+fromDual :: T (Tensor H H) -> T (Hom H H)
+fromDual = extend fromDual'
+  where
+    fromDual' :: Tensor H H -> T (Hom H H)
+    fromDual' (Tensor x E) = hom $ em $ Hom E x
+    fromDual' (Tensor x y) = scale (-1) $ hom $ em $ Hom y x
+
+
+
+showMap :: (Show z, Show w) => (H -> H -> z) -> (z -> w) -> IO ()
+showMap combine transform = do
+  let showLine (x, y) =
+         let xy = combine x y
+         in  show xy ++ "  -->  " ++ show (transform xy)
+      hs = elements :: [H]
+  mapM_ (putStrLn . showLine) [ (x,y) | x <- hs, y <- hs]
 
 showBr :: IO ()
-showBr = do
-  let showLine (x, y) =
-         let xy = return (Tensor x y) :: T (Tensor H H)
-         in  show xy ++ "  -->  " ++ show (br xy)
-      hs = elements :: [H]
-  mapM_ (putStrLn . showLine) [ (x,y) | x <- hs, y <- hs]
+showBr = showMap (\x -> \y -> return (Tensor x y) :: T (Tensor H H)) br
 
 showInvBr :: IO ()
-showInvBr = do
-  let showLine (x, y) =
-         let xy = scale 4 $ return (Hom x y) :: T (Hom H H)
-         in  show xy ++ "  -->  " ++ show (invbr xy)
-      hs = elements :: [H]
-  mapM_ (putStrLn . showLine) [ (x,y) | x <- hs, y <- hs]
+showInvBr = showMap (\x -> \y -> scale 4 $ return (Hom x y) :: T (Hom H H)) invbr
+
+showBrDual :: IO ()
+showBrDual = showMap (\x -> \y -> return (Tensor x y) :: T (Tensor H H)) (toDual . br)
+
+showInvBrDual :: IO ()
+showInvBrDual = showMap (\x -> \y -> scale 4 $ return (Tensor x y) :: T (Tensor H H)) (invbr . fromDual)
 
 
 main :: IO ()
