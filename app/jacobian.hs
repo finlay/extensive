8a09856105229245bbd2dfddaca8abdde3ae77b1
Finlay Thompson <finlay@dragonfly.co.nz>
2023-08-14 07:57:32 +1200

    Got jacobian to load

---
+++ app/jacobian.hs
@@ -0,0 +1,81 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE DataKinds #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns -Wno-orphans -Wno-unused-top-binds #-}
+
+import Prelude hiding ((+), (-), (*), (^), negate, (>), (<), sum, fromInteger)
+import Numeric.Extensive
+import Numeric.Quaternion
+
+
+br :: T (Tensor H H) -> T (Hom H H)
+br = extend $ hom . br'
+  where
+    br' :: Tensor H H -> T H -> T H
+    br' (Tensor x y) tz = let tx = return x
+                              ty = return y
+                          in  tx * tz * ty
+
+invbr :: T (Hom H H) -> T (Tensor H H)
+invbr = inverse br
+
+-- map Hom H H to Tensor H H*, where we equate H* with H (conjugated)
+toDual :: T (Hom H H) -> T (Tensor H H)
+toDual = extend toDual'
+  where
+    toDual' :: Hom H H -> T (Tensor H H)
+    toDual' (Hom E x) = return $ Tensor x E
+    toDual' (Hom y x) = scale (-1) $ return $ Tensor x y
+
+fromDual :: T (Tensor H H) -> T (Hom H H)
+fromDual = extend fromDual'
+  where
+    fromDual' :: Tensor H H -> T (Hom H H)
+    fromDual' (Tensor x E) = hom $ em $ Hom E x
+    fromDual' (Tensor x y) = scale (-1) $ hom $ em $ Hom y x
+
+
+
+showMap :: (Show z, Show w) => (H -> H -> z) -> (z -> w) -> IO ()
+showMap combine transform = do
+  let showLine (x, y) =
+         let xy = combine x y
+         in  show xy ++ "  -->  " ++ show (transform xy)
+      hs = elements :: [H]
+  mapM_ (putStrLn . showLine) [ (x,y) | x <- hs, y <- hs]
+
+showBr :: IO ()
+showBr = showMap (\x -> \y -> return (Tensor x y) :: T (Tensor H H)) br
+
+showInvBr :: IO ()
+showInvBr = showMap (\x -> \y -> scale 4 $ return (Hom x y) :: T (Hom H H)) invbr
+
+showBrDual :: IO ()
+showBrDual = showMap (\x -> \y -> return (Tensor x y) :: T (Tensor H H)) (toDual . br)
+
+showInvBrDual :: IO ()
+showInvBrDual = showMap (\x -> \y -> scale 4 $ return (Tensor x y) :: T (Tensor H H)) (invbr . fromDual)
+
+
+main :: IO ()
+main = do
+  putStrLn "Brauer map"
+  showBr
+  putStrLn "Inverse Brauer map"
+  showInvBr
+
+
+-- Show all full algebra of H Tensor H
+showHTH :: IO ()
+showHTH = do
+  let showLine (x,y) = show x ++ " × " ++ show y ++ " = " ++ show (x * y)
+  mapM_ (putStrLn . showLine) [ (x, y) | x <- tau, y <- tau ]
+
+
+showHTH2 :: IO ()
+showHTH2 = do
+  let showLine (x,y) = show x ++ " × " ++ show y ++ " = " ++ show (x * y - y * x)
+  mapM_ (putStrLn . showLine) [ (x, y) | x <- tau, y <- tau ]
+
+
+
