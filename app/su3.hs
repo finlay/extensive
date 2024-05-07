8afe483a2420ffd96f45d8f9355108d5d72d6dc0
Finlay Thompson <finlay@dragonfly.co.nz>
2024-05-07 17:53:50 +1200

    Adde dsu3 from scratch

--- app/su3.hs
+++ app/su3.hs
@@ -441,3 +441,133 @@ showER5 l =
                            <> " -> " <> (show $ rho5 l t)
   in  mapM_ (putStrLn . showres) r5
 
+--------------------------------------------------------------------------------
+-- Directly building out su3 directly for generators
+
+-- We start with a nine dimensional algebra, because it makes more sense (sort of)
+-- Even though the algebra is really eight dimensional
+
+data SU3 = Tx | Ty | Tz | Ux | Uy | Uz | Vx | Vy | Vz deriving (Ord, Eq)
+instance Order SU3 where
+    order a b = Just (compare a b)
+instance Show SU3 where
+    show Tx = "tx"
+    show Ty = "ty"
+    show Tz = "tz"
+    show Ux = "ux"
+    show Uy = "uy"
+    show Uz = "uz"
+    show Vx = "vx"
+    show Vy = "vy"
+    show Vz = "vz"
+
+instance FiniteSet SU3 where
+    elements = [ Tx, Ty, Tz, Ux, Uy, Uz, Vx, Vy, Vz ]
+
+su3:: [T SU3]
+su3 = map return elements
+tx, ty, tz, ux, uy, uz, vx, vy, vz :: T SU3
+[tx, ty, tz, ux, uy, uz, vx, vy, vz] = su3
+
+su3_bracket :: T (Tensor SU3 SU3) -> T SU3
+su3_bracket = extend su3_bracket'
+  where
+    su3_bracket' :: Tensor SU3 SU3 -> T SU3
+
+    su3_bracket' (Tx `Tensor` Ty) = tz
+    su3_bracket' (Ty `Tensor` Tx) = minus tz
+    su3_bracket' (Ty `Tensor` Tz) = tx
+    su3_bracket' (Tz `Tensor` Ty) = minus tx
+    su3_bracket' (Tz `Tensor` Tx) = ty
+    su3_bracket' (Tx `Tensor` Tz) = minus ty
+
+    su3_bracket' (Tx `Tensor` Ux) = scale (  0.5) vy
+    su3_bracket' (Tx `Tensor` Uy) = scale (- 0.5) vx
+    su3_bracket' (Tx `Tensor` Uz) = scale (  0.5) ty
+    su3_bracket' (Ty `Tensor` Ux) = scale (- 0.5) vx
+    su3_bracket' (Ty `Tensor` Uy) = scale (  0.5) vy
+    su3_bracket' (Ty `Tensor` Uz) = scale (- 0.5) tx
+    su3_bracket' (Tz `Tensor` Ux) = scale (- 0.5) uy
+    su3_bracket' (Tz `Tensor` Uy) = scale (  0.5) ux
+
+    su3_bracket' (Tx `Tensor` Vx) = scale (  0.5) uy
+    su3_bracket' (Tx `Tensor` Vy) = scale (- 0.5) ux
+    su3_bracket' (Tx `Tensor` Vz) = scale (- 0.5) ty
+    su3_bracket' (Ty `Tensor` Vx) = scale (- 0.5) ux
+    su3_bracket' (Ty `Tensor` Vy) = scale (  0.5) uy
+    su3_bracket' (Ty `Tensor` Vz) = scale (  0.5) tx
+    su3_bracket' (Tz `Tensor` Vx) = scale (  0.5) vy
+    su3_bracket' (Tz `Tensor` Vy) = scale (- 0.5) vx
+
+    su3_bracket' (Ux `Tensor` Uy) = tz
+    su3_bracket' (Uy `Tensor` Ux) = minus tz
+    su3_bracket' (Uy `Tensor` Uz) = tx
+    su3_bracket' (Uz `Tensor` Uy) = minus tx
+    su3_bracket' (Uz `Tensor` Ux) = ty
+    su3_bracket' (Ux `Tensor` Uz) = minus ty
+
+    su3_bracket' (Ux `Tensor` Tx) = scale (- 0.5) vy
+    su3_bracket' (Ux `Tensor` Ty) = scale (  0.5) vx
+    su3_bracket' (Ux `Tensor` Tz) = scale (  0.5) uy
+    su3_bracket' (Uy `Tensor` Tx) = scale (  0.5) vx
+    su3_bracket' (Uy `Tensor` Ty) = scale (- 0.5) vy
+    su3_bracket' (Uy `Tensor` Tz) = scale (- 0.5) ux
+    su3_bracket' (Uz `Tensor` Tx) = scale (- 0.5) ty
+    su3_bracket' (Uz `Tensor` Ty) = scale (  0.5) tx
+
+    su3_bracket' (Ux `Tensor` Vx) = scale (- 0.5) ty
+    su3_bracket' (Ux `Tensor` Vy) = scale (  0.5) tx
+    su3_bracket' (Ux `Tensor` Vz) = scale (- 0.5) uy
+    su3_bracket' (Uy `Tensor` Vx) = scale (- 0.5) tx
+    su3_bracket' (Uy `Tensor` Vy) = scale (  0.5) ty
+    su3_bracket' (Uy `Tensor` Vz) = scale (  0.5) ux
+    su3_bracket' (Uz `Tensor` Vx) = scale (  0.5) vy
+    su3_bracket' (Uz `Tensor` Vy) = scale (- 0.5) vx
+
+    su3_bracket' (Vx `Tensor` Vy) = vz
+    su3_bracket' (Vy `Tensor` Vx) = minus vz
+    su3_bracket' (Vy `Tensor` Vz) = vx
+    su3_bracket' (Vz `Tensor` Vy) = minus vx
+    su3_bracket' (Vz `Tensor` Vx) = vy
+    su3_bracket' (Vx `Tensor` Vz) = minus vy
+
+    su3_bracket' (Vx `Tensor` Tx) = scale (- 0.5) uy
+    su3_bracket' (Vx `Tensor` Ty) = scale (  0.5) ux
+    su3_bracket' (Vx `Tensor` Tz) = scale (- 0.5) vy
+    su3_bracket' (Vy `Tensor` Tx) = scale (  0.5) ux
+    su3_bracket' (Vy `Tensor` Ty) = scale (- 0.5) uy
+    su3_bracket' (Vy `Tensor` Tz) = scale (  0.5) vx
+    su3_bracket' (Vz `Tensor` Tx) = scale (  0.5) ty
+    su3_bracket' (Vz `Tensor` Ty) = scale (- 0.5) tx
+
+    su3_bracket' (Vx `Tensor` Ux) = scale (  0.5) ty
+    su3_bracket' (Vx `Tensor` Uy) = scale (  0.5) tx
+    su3_bracket' (Vx `Tensor` Uz) = scale (- 0.5) vy
+    su3_bracket' (Vy `Tensor` Ux) = scale (- 0.5) tx
+    su3_bracket' (Vy `Tensor` Uy) = scale (- 0.5) ty
+    su3_bracket' (Vy `Tensor` Uz) = scale (  0.5) vx
+    su3_bracket' (Vz `Tensor` Ux) = scale (  0.5) uy
+    su3_bracket' (Vz `Tensor` Uy) = scale (- 0.5) ux
+
+    su3_bracket' _ = zero
+
+instance Multiplicative (T SU3) where
+    (*) x' y' = su3_bracket (x' `tensor` y')
+
+check_sym_su3 :: (Show a, Eq a, FiniteSet a) => [T a] -> (T a -> T a -> T a) -> IO ()
+check_sym_su3 els brac = mapM_ (putStrLn . show) [ brac a b + brac b a | a <- els, b <- els ]
+
+
+jacobi :: Additive t => (t -> t -> t) -> t -> t -> t -> t
+jacobi brac a b c = brac (brac a b) c + brac (brac b c) a + brac (brac c a) b
+
+check_jac_su3 :: (Show a, Eq a, FiniteSet a) => [T a] -> (T a -> T a -> T a) -> IO ()
+check_jac_su3 els brac =
+  let combinations = [ (a, b, c, jacobi brac a b c )
+                     | a <- els, b <- els, c <- els
+                     ]
+      is_zero (_,_,_,r) = r == zero
+      okay = (length $ filter (not . is_zero) combinations) == zero
+  in  if okay
+        then putStrLn "Satisfies the Jacobi identity"
+        else mapM_ (putStrLn . show) $ filter (not . is_zero) combinations
